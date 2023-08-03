ca1f1afd0e7821777c0f1df75f9155806edd231b
Pranay1010 <37022754+Pranay1010@users.noreply.github.com>
2023-08-03 12:01:15 -0500

    Delete Snowflake directory

--- Snowflake/stage/EMPLOYEE_SALARY_STAGE.sql
+++
@@ -1,3 +0,0 @@
--- Stage for loading EMPLOYEE_SALARY_DATA data from a local directory
-CREATE OR REPLACE STAGE USER_DB_DEV.EMPLOYEE_DATA.EMPLOYEE_SALARY_STAGE
-URL = 'C:///Users/kpran/Snowflake/directory/employee';
--- Snowflake/task/Task_GetEmployeeDetailsWithAttributes.sql
+++
@@ -1,6 +0,0 @@
--- Create Tasks
-CREATE OR REPLACE TASK Task_GetEmployeeDetailsWithAttributes
-    WAREHOUSE = PRANAY_DB_DEV
-    SCHEDULE = 'EVERY 1 HOUR'
-AS
-CALL GetEmployeeDetailsWithAttributes();
