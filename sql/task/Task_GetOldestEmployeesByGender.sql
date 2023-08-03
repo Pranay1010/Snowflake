383596ffcd12905bc3bcfb84276253be59c049a2
Pranay1010 <37022754+Pranay1010@users.noreply.github.com>
2023-08-03 11:29:46 -0500

    Delete sql directory

--- sql/task/Task_GetOldestEmployeesByGender.sql
+++
@@ -1,6 +0,0 @@
--- Create task
-CREATE OR REPLACE TASK Task_GetOldestEmployeesByGender
-    WAREHOUSE = PRANAY_DB_DEV
-    SCHEDULE = 'EVERY 1 DAY'
-AS
-CALL GetOldestEmployeesByGender();
--- sql/task/Task_GetTopEarnersByCountry.sql
+++
@@ -1,6 +0,0 @@
--- Create task
-CREATE OR REPLACE TASK Task_GetTopEarnersByCountry
-    WAREHOUSE = PRANAY_DB_DEV
-    SCHEDULE = 'EVERY 1 DAY'
-AS
-CALL GetTopEarnersByCountry();
